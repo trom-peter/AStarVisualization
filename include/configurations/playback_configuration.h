e48adbcdd217aea4e0383f96f52e4db1d0031dd2
Luis <luis.bruhn@stud.th-luebeck.de>
2025-08-11 21:34:59 +0200

    added comments

--- include/configurations/playback_configuration.h
+++ include/configurations/playback_configuration.h
@@ -7,9 +7,9 @@ struct PlaybackConfig {
 	void updateTimePerIncrement();
 
 	int step; // step number in search process. measured in number of expanded nodes
-	int maxSteps;
+	int maxSteps; // maximum steps in search process
 	float searchTime;
-	int searchRate;
+	int searchRate; //steps per second
 	bool searchPlaying;
 	float timePerIncrement;
 };

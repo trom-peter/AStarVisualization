3d7efde4cc846937eda2d1ad1acec328c489214a
Luis <luis.bruhn@stud.th-luebeck.de>
2025-08-19 20:08:54 +0200

    node uses weak ptr for parent

--- include/model/node.h
+++ include/model/node.h
@@ -4,15 +4,13 @@
 #include "model/state.h"
 
 struct Node {
-	Node(const int x, const int y, const int z, const int pathCost, std::shared_ptr<Node> parent);
+	Node(const int x, const int y, const int z, const int pathCost, std::weak_ptr<Node> parent);
 	Node(const int x, const int y, const int z, const int pathCost);
-	Node(const int x, const int y, const int z, std::shared_ptr<Node> parent, const int pathCost);
+	Node(const int x, const int y, const int z, std::weak_ptr<Node> parent, const int pathCost);
 	Node(const State state, const int pathCost);
-	Node(const State state, std::shared_ptr<Node> parent, const int pathCost);
+	Node(const State state, std::weak_ptr<Node> parent, const int pathCost);
 
-	std::string getPath() const;
-
-	std::shared_ptr<Node> parent;
+	std::weak_ptr<Node> parent;
 	State s;
 	int pathCost; // Travel time in seconds
 };
--- source/model/node.cpp
+++ source/model/node.cpp
@@ -2,36 +2,23 @@
 #include "model/state.h"
 #include <string>
 
-Node::Node(const int x, const int y, const int z, const int pathCost, std::shared_ptr<Node> parent) :
+Node::Node(const int x, const int y, const int z, const int pathCost, std::weak_ptr<Node> parent) :
 	s(State(x, y, z)), parent(parent), pathCost(pathCost) {
 };
 
 Node::Node(const int x, const int y, const int z, const int pathCost) :
-	s(State(x, y, z)), parent(nullptr), pathCost(pathCost) {
+	s(State(x, y, z)), pathCost(pathCost) {
 };
 
-Node::Node(const int x, const int y, const int z, std::shared_ptr<Node> parent, const int pathCost) :
+Node::Node(const int x, const int y, const int z, std::weak_ptr<Node> parent, const int pathCost) :
 	s(State(x, y, z)), parent(parent), pathCost(pathCost) {
 };
 
 Node::Node(const State state, const int pathCost) :
-	s(state), parent(nullptr), pathCost(pathCost) {
+	s(state), pathCost(pathCost) {
 };
 
-Node::Node(const State state, std::shared_ptr<Node> parent, const int pathCost) :
+Node::Node(const State state, std::weak_ptr<Node> parent, const int pathCost) :
 	s(state), parent(parent), pathCost(pathCost) {
 };
 
-std::string Node::getPath() const {
-	if (parent == nullptr) {
-		return "[" + std::to_string(s.x) + ", " + 
-			std::to_string(s.y) + ", " + 
-			std::to_string(s.z) + "]";
-	}
-	else {
-		return parent->getPath() + " -> " + 
-			"[" + std::to_string(s.x) + ", " + 
-			std::to_string(s.y) + ", " + 
-			std::to_string(s.z) + "]";
-	}
-}
